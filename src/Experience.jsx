c31a367e9cea1fbc5c2a78870164152c0cb83c62
Rafael Anguiano <rj.anguiano.23@gmail.com>
2024-03-03 21:30:33 -0600

    Added animation to the shader in the portal

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,13 +5,14 @@ import {
     Center,
     Sparkles
 } from "@react-three/drei"
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import * as THREE from "three"
 import vertexShader from "./shaders/vertex.glsl"
 import fragmentShader from "./shaders/fragment.glsl"
 import { useControls } from "leva"
 import { useFrame } from "@react-three/fiber"
 import { extend } from "@react-three/fiber"
+
 
 const PortalMaterial = shaderMaterial(
   {
@@ -26,67 +27,55 @@ const PortalMaterial = shaderMaterial(
 extend({ PortalMaterial })
 
 const Experience = () => {
+  const portalMaterial = useRef()
+  const { nodes } = useGLTF('./model/portal.glb')
+  const bakedTexture = useTexture('./model/baked.jpg')
+  bakedTexture.flipY = false
 
-    const { nodes } = useGLTF('./model/portal.glb')
+  useFrame( (state, delta) => {
+    portalMaterial.current.uTime += delta
+  })
 
-    const bakedTexture = useTexture('./model/baked.jpg')
-    bakedTexture.flipY = false
+  return <>
+      <Center>
+          <mesh geometry={nodes.baked.geometry}>
+              <meshBasicMaterial map={bakedTexture} />
+          </mesh>
 
-    const { colorStart, colorEnd } = useControls({
-        colorStart: {
-            value: "#ff0055",
-            onChange: (value) => {
-                nodes.portalLight.material.color = new THREE.Color(value)
-            }
-        },
-        colorEnd: {
-            value: "#ffaa00",
-            onChange: (value) => {
-                nodes.portalLight.material.color = new THREE.Color(value)
-            }
-        }
-    })
+          <mesh
+              geometry={nodes.poleLightA.geometry}
+              position={nodes.poleLightA.position}
+              rotation={nodes.poleLightA.rotation}
+              scale={nodes.poleLightA.scale}
+          >
+              <meshBasicMaterial color={"#ffffe5"} />
+          </mesh>
+          <mesh
+              geometry={nodes.poleLightB.geometry}
+              position={nodes.poleLightB.position}
+              rotation={nodes.poleLightB.rotation}
+              scale={nodes.poleLightB.scale}
+          >
+              <meshBasicMaterial color={"#ffffe5"} />
+          </mesh>
+          <mesh
+              geometry={nodes.portalLight.geometry}
+              position={nodes.portalLight.position}
+              rotation={nodes.portalLight.rotation}
+              scale={nodes.portalLight.scale}
+          >
+            <portalMaterial ref={portalMaterial} />
+          </mesh>
 
-    return <>
-        <Center>
-            <mesh geometry={nodes.baked.geometry}>
-                <meshBasicMaterial map={bakedTexture} />
-            </mesh>
-
-            <mesh
-                geometry={nodes.poleLightA.geometry}
-                position={nodes.poleLightA.position}
-                rotation={nodes.poleLightA.rotation}
-                scale={nodes.poleLightA.scale}
-            >
-                <meshBasicMaterial color={"#ffffe5"} />
-            </mesh>
-            <mesh
-                geometry={nodes.poleLightB.geometry}
-                position={nodes.poleLightB.position}
-                rotation={nodes.poleLightB.rotation}
-                scale={nodes.poleLightB.scale}
-            >
-                <meshBasicMaterial color={"#ffffe5"} />
-            </mesh>
-            <mesh
-                geometry={nodes.portalLight.geometry}
-                position={nodes.portalLight.position}
-                rotation={nodes.portalLight.rotation}
-                scale={nodes.portalLight.scale}
-            >
-              <portalMaterial />
-            </mesh>
-
-            <Sparkles
-                size={3}
-                scale={[3.5, 1, 3.5]}
-                position-y={1}
-                speed={0.8}
-                count={20}
-            />
-        </Center>
-    </>
+          <Sparkles
+              size={3}
+              scale={[3.5, 1, 3.5]}
+              position-y={1}
+              speed={0.8}
+              count={20}
+          />
+      </Center>
+  </>
 }
 
 export default Experience
